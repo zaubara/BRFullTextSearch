7ed15d686c98e6ab37523656b9bda1a5e026ce24
Martin Puza <m.puza@zetes.at>
2016-12-04 20:00:13 +0100

    included pods project added ios framework

---
+++ BRFullTextSearch Framework iOS/BRFullTextSearchFramework.h
@@ -0,0 +1,28 @@
+//
+//  BRFullTextSearchFramework.h
+//  BRFullTextSearch
+//
+//  Created by Martin Puza on 04.12.16.
+//  Copyright © 2016 Blue Rocket. All rights reserved.
+//
+
+#ifndef BRFullTextSearchFramework_h
+#define BRFullTextSearchFramework_h
+
+//
+//  BRFullTextSearch.h
+//  BRFullTextSearch
+//
+//  Created by Matt on 7/3/13.
+//  Copyright (c) 2013 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
+//
+
+#import <BRFullTextSearch/BRIndexable.h>
+#import <BRFullTextSearch/BRIndexUpdateContext.h>
+#import <BRFullTextSearch/BRSearchFields.h>
+#import <BRFullTextSearch/BRSearchResults.h>
+#import <BRFullTextSearch/BRSearchService.h>
+#import <BRFullTextSearch/BRSimpleIndexable.h>
+
+
+#endif /* BRFullTextSearchFramework_h */
